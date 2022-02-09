bd4e6c8cdbfd7c28bebeff9fcbda6cf12b67394c
HHY1079 <97010072+HHY1079@users.noreply.github.com>
2022-02-09 21:24:59 +0900

    Add files via upload

---
+++ colors.js
@@ -0,0 +1,33 @@
+var Links = {
+  SetColor(color){
+    var alist = document.querySelectorAll('a');
+    var i = 0;
+    while(i<alist.length){
+      alist[i].style.color = color;
+      i = i + 1
+    }
+  }
+}
+var Body = {
+  SetBackgroundColor(color){
+    document.querySelector('body').style.backgroundColor = color;
+  },
+  SetColor(color){
+    document.querySelector('body').style.color = color;
+  }
+}
+
+function NightDayHandler(self){
+  var target = document.querySelector('body')
+  if(self.value === 'night'){
+    Body.SetBackgroundColor('black');
+    Body.SetColor('white');
+    self.value = 'day';
+    Links.SetColor('gray');
+  } else {
+    Body.SetBackgroundColor('white');
+    Body.SetColor('black');
+    self.value = 'night';
+    Links.SetColor('black');
+  }
+}
